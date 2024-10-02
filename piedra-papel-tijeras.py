c55475a83f30b08ac41928af0484c7e23f1f3f48
Bzg73 <106202493+Bzg73@users.noreply.github.com>
2024-10-02 01:39:41 -0300

    Update piedra-papel-tijeras.py

--- piedra-papel-tijeras.py
+++ piedra-papel-tijeras.py
@@ -1,8 +1,8 @@
 nombre1 = input("¿nombre del jugador 1?: ")
 nombre2 = input("¿nombre del jugador 2?: ")
 
-jugador1 = input("Jugador 1! ¿Qué eliges?¿Piedra,papel, o tijera?: ")
-jugador2 = input("Jugador 2! ¿Qué eliges?¿Piedra,papel, o tijera?: ")
+jugador1 = input(f"{nombre1}, ¿Qué eliges?¿Piedra,papel, o tijera?: ").lower()
+jugador2 = input(f"{nombre2}, ¿Qué eliges?¿Piedra,papel, o tijera?: ").lower()
 
 condicion1 = jugador1 == "piedra" and jugador2 == "tijera"
 condicion2 = jugador1 == "papel" and jugador2 == "piedra"
@@ -11,7 +11,7 @@ condicion3 = jugador1 == "tijera" and jugador2 == "papel"
 if jugador1 == jugador2:
     print("EMPATE!")
 elif condicion1 or condicion2 or condicion3:
-    print("GANO: ", nombre1)
+    print(f"GANO {nombre1}")
 
 else:
-    print ("GANO: ", nombre2) 
+    print (f"GANO {nombre2}") 
